2f0d000401675e759ec275d212746887fb6326bd
MrFreyd <antosha.konkov@mail.ru>
2020-12-06 14:01:15 +0700

    compleate adaptive news block

--- js/index.js
+++ js/index.js
@@ -78,23 +78,18 @@ if(this.id == 'red'){
 */
 var element = document.getElementById('click');
 $('.navigation').hide();
-$('#twoSearch').hide()
 var click2 = 0
 element.onclick = function(){
 	click2++
-	$('#oneSearch').hide();
 	$('.navigation').show();
-	$('#twoSearch').show();
-	//$('.blockSearch').hide();
+	//$('header').style.boxshadow = 'none';
 	click = 0
 		if(click2 == 2){
 			$('.navigation').hide();
-			$('#twoSearch').hide();
 			click2 = 0;
 		}
 	$(window).resize(function() { 
 		if(document.body.clientWidth>750){
-			$('#twoSearch').hide();
 			$('.navigation').hide();
 			click2 = 0
 		}
